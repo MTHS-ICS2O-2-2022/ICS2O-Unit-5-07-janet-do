a17e0c68787b256da561d69d66264165fd52e2c3
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-11 16:39:19 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Janet Do All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+let integer = parseInt(prompt("Enter an integer: "));
+let sum = 0
+
+for (let counter = 1; counter <= integer; counter++) {
+  sum += counter
+}
+
+console.log("The sum of all numbers from 1 to " + integer + " is " + sum);
